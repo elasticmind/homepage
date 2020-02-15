5d8bb00bb0a6de2c7e1a947643842de94834fb24
Tibor Zombory <contact@elasticmind.design>
2020-02-15 15:19:39 +0100

    Add decor to primary sections

--- components/BaseSection.vue
+++ components/BaseSection.vue
@@ -1,14 +1,16 @@
 <template>
   <section
-    class="w-screen pt-12"
-    :class="[primary ? 'bg-yellow-300' : 'bg-blue-300']"
+    class="w-screen pt-12 section"
+    :class="[
+      primary ? 'bg-gray-100 text-gray-900 decor' : 'bg-teal-900 text-gray-100'
+    ]"
   >
-    <header>
+    <header class="relative z-10">
       <h1 class="text-left text-4xl bg-blue-600 w-1/4">
         <slot name="header" />
       </h1>
     </header>
-    <article class="text-left px-64 py-48 content">
+    <article class="text-left px-64 py-48 relative z-10 content">
       <slot name="content" />
     </article>
   </section>
@@ -37,5 +39,32 @@ export default {
 
   p {
   }
+}
+
+.section {
+  position: relative;
+}
+
+.decor::before,
+.decor::after {
+  content: '';
+  display: block;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.decor::before {
+  opacity: 0.1;
+  background-color: theme('colors.blue.400');
+  transform: rotateZ(2deg) scaleX(1.1);
+}
+
+.decor::after {
+  opacity: 0.1;
+  background-color: theme('colors.green.400');
+  transform: rotateZ(6deg) scaleX(1.1);
 }
 </style>
