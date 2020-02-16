e4fbc106765d527d793c4be417d8196ba7f87661
Tibor Zombory <contact@elasticmind.design>
2020-02-16 09:35:51 +0100

    Fill People section with content

--- components/People.vue
+++ components/People.vue
@@ -6,60 +6,195 @@
     <template slot="content">
       <section>
         <header>
-          <h1>
+          <h1 class="text-2xl">
             Technical
           </h1>
         </header>
-        <article>
-          <header>
-            <h2>
-              Getify
-            </h2>
-          </header>
-          <ul>
-            <li>
+        <ul class="text-base">
+          <li>
+            <header>
+              <h2 class="text-xl">
+                Kyle Simpson (Getify)
+              </h2>
+            </header>
+            <article>
               <p>
-                Contact #1_1
+                Kyle Simpson is an evangelist of the open web, passionate about
+                all things JavaScript. He writes books, teaches JavaScript,
+                speaks, and contributes to the world of OSS.
               </p>
-            </li>
-          </ul>
-          <header>
-            <h2>
-              Chris Fritz
-            </h2>
-          </header>
-          <ul>
-            <li>
+            </article>
+            <ul class="flex">
+              <li class="m-2">
+                <a href="https://getify.com/">
+                  <div class="w-full h-full rounded-full">
+                    <i
+                      class="fa fa-globe m-auto text-2xl align-middle"
+                      aria-hidden="true"
+                    >
+                    </i>
+                  </div>
+                </a>
+              </li>
+              <li class="m-2">
+                <a href="https://github.com/getify">
+                  <div class="w-full h-full rounded-full">
+                    <i
+                      class="fa fa-github m-auto text-2xl align-middle"
+                      aria-hidden="true"
+                    >
+                    </i>
+                  </div>
+                </a>
+              </li>
+              <li class="m-2">
+                <a href="mail:getify@gmail.com">
+                  <div class="w-full h-full rounded-full">
+                    <i
+                      class="fa fa-envelope m-auto text-2xl align-middle"
+                      aria-hidden="true"
+                    >
+                    </i>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <header>
+              <h2 class="text-xl">
+                Chris Fritz
+              </h2>
+            </header>
+            <article>
               <p>
-                Contact #1_1
+                Chris Fritz lives in Lansing, Michigan, working on open source
+                and helping companies around the world build incredible web
+                front-ends. He’s most well known for his work as a member of the
+                Vue core team, writing and curating Vue’s documentation, along
+                with other resources to make life simpler for web developers. As
+                a remote consultant nowhere near any coasts, he enjoys traveling
+                the world and answering the inevitable question, "So… why
+                Michigan? Are you hiding from the law or something?" When at
+                home and his lap isn’t occupied by a computer, it usually has a
+                cat on it &ndash; or that may be the other way around.
               </p>
-            </li>
-          </ul>
-          <header>
-            <h2>
-              Mark Oberman
-            </h2>
-          </header>
-          <ul>
-            <li>
+            </article>
+            <ul class="flex">
+              <li class="m-2">
+                <a href="https://github.com/chrisvfritz">
+                  <div class="w-full h-full rounded-full">
+                    <i
+                      class="fa fa-github m-auto text-2xl align-middle"
+                      aria-hidden="true"
+                    >
+                    </i>
+                  </div>
+                </a>
+              </li>
+              <li class="m-2">
+                <a href="mail:chrisvfritz@gmail.com">
+                  <div class="w-full h-full rounded-full">
+                    <i
+                      class="fa fa-envelope m-auto text-2xl align-middle"
+                      aria-hidden="true"
+                    >
+                    </i>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <header>
+              <h2 class="text-xl">
+                Markus Oberlehner
+              </h2>
+            </header>
+            <article>
               <p>
-                Contact #1_1
+                Markus Oberlehner is a Web Developer, specializing in Vue. He's
+                a passionate about Open Source and he's the creator of the
+                vuex-map-fields, vue-lazy-hydration, and
+                node-sass-magic-importer packages.
               </p>
-            </li>
-          </ul>
-          <header>
-            <h2>
-              chris heimlich
-            </h2>
-          </header>
-          <ul>
-            <li>
+            </article>
+            <ul class="flex">
+              <li class="m-2">
+                <a href="https://markus.oberlehner.net/">
+                  <div class="w-full h-full rounded-full">
+                    <i
+                      class="fa fa-globe m-auto text-2xl align-middle"
+                      aria-hidden="true"
+                    >
+                    </i>
+                  </div>
+                </a>
+              </li>
+              <li class="m-2">
+                <a href="https://github.com/maoberlehner">
+                  <div class="w-full h-full rounded-full">
+                    <i
+                      class="fa fa-github m-auto text-2xl align-middle"
+                      aria-hidden="true"
+                    >
+                    </i>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <header>
+              <h2 class="text-xl">
+                Christian Heilmann
+              </h2>
+            </header>
+            <article>
               <p>
-                Contact #1_1
+                Chris is a technical and open web evangelist. "Maker of web
+                things. Ponderer of new technology. Lover of simplicity."
+                &ndash; as he puts it. He's a veteran web developer, publisher,
+                speaker and conference organiser.
               </p>
-            </li>
-          </ul>
-        </article>
+            </article>
+            <ul class="flex">
+              <li class="m-2">
+                <a href="https://christianheilmann.com/">
+                  <div class="w-full h-full rounded-full">
+                    <i
+                      class="fa fa-globe m-auto text-2xl align-middle"
+                      aria-hidden="true"
+                    >
+                    </i>
+                  </div>
+                </a>
+              </li>
+              <li class="m-2">
+                <a href="http://github.com/codepo8">
+                  <div class="w-full h-full rounded-full">
+                    <i
+                      class="fa fa-github m-auto text-2xl align-middle"
+                      aria-hidden="true"
+                    >
+                    </i>
+                  </div>
+                </a>
+              </li>
+              <li class="m-2">
+                <a href="mail:chris@christianheilmann.com">
+                  <div class="w-full h-full rounded-full">
+                    <i
+                      class="fa fa-envelope m-auto text-2xl align-middle"
+                      aria-hidden="true"
+                    >
+                    </i>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
       </section>
       <section>
         <header>
@@ -67,32 +202,105 @@
             Non-Technical
           </h1>
         </header>
-        <article>
-          <header>
-            <h2>
-              Róbert Puzsér
-            </h2>
-          </header>
-          <ul>
-            <li>
+        <ul>
+          <li>
+            <header>
+              <h2 class="text-xl">
+                Róbert Puzsér
+              </h2>
+            </header>
+            <article>
               <p>
-                Contact #1_1
+                Róbert Puzsér is a Hungarian publicist, anchorman, editor, and
+                social critic. He is one of the most divisive intellectual in
+                the Hungarian cultural space. He has also ran for the position
+                of Lord Mayor of Budapest in 2019, with a modern and green
+                political program to steer Budapest towards a more sustainable,
+                more human-friendly direction.
               </p>
-            </li>
-          </ul>
-          <header>
-            <h2>
-              Jordan Peterson
-            </h2>
-          </header>
-          <ul>
-            <li>
+            </article>
+            <ul class="flex">
+              <li class="m-2">
+                <a href="http://puzser.hu/">
+                  <div class="w-full h-full rounded-full">
+                    <i
+                      class="fa fa-globe m-auto text-2xl align-middle"
+                      aria-hidden="true"
+                    >
+                    </i>
+                  </div>
+                </a>
+              </li>
+              <li class="m-2">
+                <a
+                  href="https://www.youtube.com/channel/UC7ivXUYaRoZb5WDLKksETUA"
+                >
+                  <div class="w-full h-full rounded-full">
+                    <i
+                      class="fa fa-youtube m-auto text-2xl align-middle"
+                      aria-hidden="true"
+                    >
+                    </i>
+                  </div>
+                </a>
+              </li>
+              <li class="m-2">
+                <a href="https://www.facebook.com/Robert.Puzser">
+                  <div class="w-full h-full rounded-full">
+                    <i
+                      class="fa fa-facebook m-auto text-2xl align-middle"
+                      aria-hidden="true"
+                    >
+                    </i>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <header>
+              <h2 class="text-xl">
+                Jordan Peterson
+              </h2>
+            </header>
+            <article>
               <p>
-                Contact #1_1
+                Jordan Peterson is a Canadian clinical psychologist and a
+                professor of psychology at the University of Toronto. He's the
+                author of Maps of Meaning: The Architecture of Belief, and 12
+                Rules for Life: An Antidote to Chaos. He actively criticizes
+                certain political extremities and is always willing to describe
+                and analyze cultural problems in their full spectrum and depth.
+                He maintains an active presence on YouTube, with hundreds of
+                hours of lectures.
               </p>
-            </li>
-          </ul>
-        </article>
+            </article>
+            <ul class="flex">
+              <li class="m-2">
+                <a href="https://www.jordanbpeterson.com/">
+                  <div class="w-full h-full rounded-full">
+                    <i
+                      class="fa fa-globe m-auto text-2xl align-middle"
+                      aria-hidden="true"
+                    >
+                    </i>
+                  </div>
+                </a>
+              </li>
+              <li class="m-2">
+                <a href="https://www.youtube.com/user/JordanPetersonVideos">
+                  <div class="w-full h-full rounded-full">
+                    <i
+                      class="fa fa-youtube m-auto text-2xl align-middle"
+                      aria-hidden="true"
+                    >
+                    </i>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
       </section>
     </template>
   </base-section>
