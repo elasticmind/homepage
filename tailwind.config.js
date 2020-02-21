0f16314f857e8439f5071ec8f33764745c864800
Tibor Zombory <contact@elasticmind.design>
2020-02-21 11:52:02 +0100

    Add decorations to BaseSection

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,31 @@ module.exports = {
       '0': '0',
       '1/2': '50%'
     },
+    padding: {
+      '2': '0.5rem',
+      '4': '1rem',
+      '6': '1.5rem',
+      '8': '2rem',
+      '12': '3rem',
+      '16': '4rem',
+      '24': '6rem',
+      '32': '8rem',
+      '48': '12rem',
+      '96': '24rem'
+    },
     minHeight: {
       '200': '200px'
+    },
+    boxShadow: {
+      default: '0 2px 4px 0 rgba(0,0,0,0.4)',
+      inner: 'inset 0 2px 4px 0 rgba(0,0,0,0.4)'
     }
   },
-  variants: {},
+  variants: {
+    margin: ['hover'],
+    padding: ['hover'],
+    borderColor: ['hover'],
+    borderWidth: ['hover']
+  },
   plugins: []
 }
